3930019f9f5d5b1fed905de80aaa93dcb318ff6c
Gnis97 <49316776+Gnis97@users.noreply.github.com>
2019-12-15 21:18:20 +0100

    casi terminado

--- practica8.cpp
+++ practica8.cpp
@@ -30,28 +30,28 @@ static GLuint texturaCarretera, texturaFondoD, texturaFondoN, texturaPepsi;
 float velocidad, angulo;
 float posx, girox, posz, giroz;
 
-bool noche = false;
+bool noche = false, niebla = false;
 
 //Luz de luna
-GLfloat Al0[] = { 0.05f ,0.05f ,0.05f ,1.0 };
-GLfloat Dl0[] = { 0.05f ,0.05f ,0.05f ,1.0 };
-GLfloat Sl0[] = { 0.0f , 0.0f ,0.0f, 1.0 };
-GLfloat directionalPosition[] = { 0.0f ,10.0f ,0.0f ,0.0f };
+GLfloat Al0[] = { 0.05 ,0.05 ,0.05 ,1.0 };
+GLfloat Dl0[] = { 0.05 ,0.05 ,0.05 ,1.0 };
+GLfloat Sl0[] = { 0.0 , 0.0 ,0.0, 1.0 };
+GLfloat directionalPosition[] = { 0.0 ,10.0 ,0.0 ,0.0 };
 //Luz frontal-CascoMinero
-GLfloat Al1[] = { 0.2f ,0.2f ,0.2f, 1.0 };
-GLfloat Dl1[] = { 1.0f, 1.0f, 1.0f, 1.0 };
-GLfloat Sl1[] = { 0.3f, 0.3f, 0.3f, 1.0 };
+GLfloat Al1[] = { 0.2 ,0.2 ,0.2, 1.0 };
+GLfloat Dl1[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat Sl1[] = { 0.3, 0.3, 0.3, 1.0 };
 //Luz farolas
-GLfloat Al2[] = { 0.0f, 0.0f, 0.0f, 1.0 };
-GLfloat Dl2[] = { 0.5f, 0.5f, 0.2f, 1.0 };
-GLfloat Sl2[] = { 0.0f, 0.0f, 0.0f, 1.0 };
-GLfloat directionalFarola[] = { 0.0f, -1.0f, 0.0f, 0.0f };
+GLfloat Al2[] = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat Dl2[] = { 0.5, 0.5, 0.2, 1.0 };
+GLfloat Sl2[] = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat directionalFarola[] = { 0.0f, -1.0, 0.0, 0.0 };
 //difuse y Specular de la carretera
-GLfloat Dm[] = { 0.8f, 0.8f, 0.8f, 1.0 };
-GLfloat Sm[] = { 0.3f, 0.3f, 0.3f, 1.0 };
+GLfloat Dm[] = { 0.8, 0.8, 0.8, 1.0 };
+GLfloat Sm[] = { 0.3, 0.3, 0.3, 1.0 };
 
-GLfloat directionalView[] = { 0.0f, -0.5f, -0.7f };
-GLfloat posicionFocal[] = { 0.0f, 0.7f, 0.0f, 1.0f };
+GLfloat directionalView[] = { 0.0, -0.8, -1.4 };
+GLfloat posicionFocal[] = { 0.0, 0.7, 0.0, 1.0 };
 
 float deriv(float u)
 {
@@ -81,7 +81,6 @@ float *vectorN(float u)
 
 void crearPepsis() {
 	//calculamos la posicion relativa
-	glBindTexture(GL_TEXTURE_2D, texturaPepsi);
 	if (noche) {
 		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 	}
@@ -89,31 +88,25 @@ void crearPepsis() {
 		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
 	}
-	float despFar = fmod(posx, 40.0f);
+	float despFar = fmod(posx, 70.0f);
 	float xFirst = posx - despFar;
 
 	for (int j = 0; j < 4; j++) {//Creamos 4 pepsis
-		float x = xFirst + 40 * j;
-		float valorfunc = funcion(x-1);
-		GLfloat posPepsi4[3] = { x, 5, valorfunc - (AnchoCar / 2) };
-		GLfloat posPepsi3[3] = { x, 5, valorfunc + (AnchoCar / 2) };
-		GLfloat posPepsi2[3] = { x, 2.5, valorfunc + (AnchoCar / 2) };
-		GLfloat posPepsi1[3] = { x, 2.5, valorfunc  - (AnchoCar / 2) };
-		switch (j)
-		{
-		case 0:
-			quadtex(posPepsi1, posPepsi2, posPepsi3, posPepsi4);
-			break;
-		case 1:
-			quadtex(posPepsi1, posPepsi2, posPepsi3, posPepsi4);
-			break;
-		case 2:
-			quadtex(posPepsi1, posPepsi2, posPepsi3, posPepsi4);
-			break;
-		case 3:
-			quadtex(posPepsi1, posPepsi2, posPepsi3, posPepsi4);
-			break;
-		}
+		float x = xFirst + 70 * j;
+		float valorfunc = funcion(x);
+		float mitad = valorfunc - AnchoCar;
+		GLfloat posPepsi1[3] = { x, 7, mitad + (AnchoCar / 2) };
+		GLfloat posPepsi2[3] = { x, 7, mitad - (AnchoCar / 2) };
+		GLfloat posPepsi3[3] = { x, 2.5, mitad - (AnchoCar / 2) };
+		GLfloat posPepsi4[3] = { x, 2.5, mitad + (AnchoCar / 2) };
+		GLfloat pospos1[3] = { x, 2.5, mitad + 0.2 };
+		GLfloat pospos2[3] = { x, 2.5, mitad - 0.2 };
+		GLfloat pospos3[3] = { x, 0, mitad - 0.2 };
+		GLfloat pospos4[3] = { x, 0, mitad + 0.2 };
+		glBindTexture(GL_TEXTURE_2D, texturaPepsi);
+		quadtex(posPepsi1, posPepsi2, posPepsi3, posPepsi4,0.0, 1.0, 1.0, 0.0, 20, 20);
+		glBindTexture(GL_TEXTURE_2D, texturaCarretera);
+		quadtex(pospos1, pospos2, pospos3, pospos4, 0.2, 0.4, 0.4, 0.2, 20, 20);
 	}
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
@@ -165,7 +158,7 @@ void crearCarretera() {
 		GLfloat v2[3] = { u2, 0, (-1 * AnchoCar) / 2 + fDe2 };
 		GLfloat v3[3] = { u, 0, (-1 * AnchoCar) / 2 + fDe1 };
 
-		quadtex(v0, v1, v2, v3, 0.0, 1.0, 1.0, 0.0, 10, 10);
+		quadtex(v0, v1, v2, v3, 0.0, 1.0, 1.0, 0.0, 20, 20);
 
 	}
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -191,12 +184,12 @@ void crearFondo() {
 }
 
 void iniciarLuces() {
-	glEnable(GL_LIGHTING);
+	glShadeModel(GL_SMOOTH);
 
 	glLightfv(GL_LIGHT0, GL_AMBIENT, Al0);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, Dl0);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, Sl0);
-	glLightfv(GL_LIGHT0, GL_POSITION, directionalPosition);
+	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, directionalPosition);
 	glEnable(GL_LIGHT0);
 
 	glLightfv(GL_LIGHT1, GL_AMBIENT, Al1);
@@ -205,6 +198,13 @@ void iniciarLuces() {
 	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 25.0f);
 	glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 20.0f);
 	glEnable(GL_LIGHT1);
+
+	glLightfv(GL_LIGHT6, GL_AMBIENT, Al1);
+	glLightfv(GL_LIGHT6, GL_DIFFUSE, Dl1);
+	glLightfv(GL_LIGHT6, GL_SPECULAR, Sl1);
+	glLightf(GL_LIGHT6, GL_SPOT_CUTOFF, 25.0f);
+	glLightf(GL_LIGHT6, GL_SPOT_EXPONENT, 20.0f);
+	glEnable(GL_LIGHT6);
 
 	glLightfv(GL_LIGHT2, GL_AMBIENT, Al2);
 	glLightfv(GL_LIGHT2, GL_DIFFUSE, Dl2);
@@ -238,10 +238,54 @@ void iniciarLuces() {
 	glLightfv(GL_LIGHT5, GL_SPOT_DIRECTION, directionalFarola);
 	glEnable(GL_LIGHT5);
 	//Definimos ls materiales de la carretera
-	glShadeModel(GL_SMOOTH);
+	
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, Dm);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, Sm);
 	glMaterialf(GL_FRONT, GL_SHININESS, 3.0);
+}
+
+void dibujaVelocidad() {
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDepthMask(GL_FALSE);
+	glPushMatrix();
+	glColor4f(1.0, 0.0, 0.0, 0.6);
+	glPolygonMode(GL_FRONT, GL_FILL);
+	glBegin(GL_POLYGON);
+	glVertex3f(-1.0, 0.0, 0.0);
+	glVertex3f(-0.99 + velocidad / 30, 0.0, 0.0);
+	glVertex3f(-0.99 + velocidad / 30, 1.0, 0.0);
+	glVertex3f(-1.0, 1.0, 0.0);
+	glEnd();
+	glPopMatrix();
+	glDepthMask(GL_TRUE);
+	glDisable(GL_BLEND);
+	glColor3f(1.0, 1.0, 1.0);
+}
+
+void dibujaFlecha(float escala) {
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDepthMask(GL_FALSE);
+	glPushMatrix();
+	glColor4f(1.0, 1.0, 0.0, 0.5);
+	glScalef(escala, escala, escala);
+	glPolygonMode(GL_FRONT, GL_FILL);
+	glBegin(GL_POLYGON);
+	glVertex3f(-0.4, -0.5, 0.0);
+	glVertex3f(0.4, -0.5, 0.0);
+	glVertex3f(0.4, 0.25, 0.0);
+	glVertex3f(-0.4, 0.25, 0.0);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex3f(-1.0, 0.25, 0.0);
+	glVertex3f(1.0, 0.25, 0.0);
+	glVertex3f(0.0, 1.0, 0.0);
+	glEnd();
+	glPopMatrix();
+	glDepthMask(GL_TRUE);
+	glDisable(GL_BLEND);
+	glColor3f(1.0, 1.0, 1.0);
 }
 
 void init()
@@ -255,7 +299,7 @@ void init()
 
 	iniciarLuces();
 
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0, 0, 0, 1);
 
 	glGenTextures(1, &texturaCarretera);
 	glBindTexture(GL_TEXTURE_2D, texturaCarretera);
@@ -298,8 +342,11 @@ void display()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	if (noche) {
+		glEnable(GL_LIGHTING);
 		glLightfv(GL_LIGHT1, GL_POSITION, posicionFocal);
 		glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, directionalView);
+		glLightfv(GL_LIGHT6, GL_POSITION, posicionFocal);
+		glLightfv(GL_LIGHT6, GL_SPOT_DIRECTION, directionalView);
 	}
 	//Colocamos la camara
 	gluLookAt(posx, 1, posz, girox, 0, giroz, 0, 1, 0);
@@ -315,7 +362,61 @@ void display()
 
 	crearCarretera();
 
+	if (niebla) {
+		glEnable(GL_FOG);
+		if (noche) {
+			glFogfv(GL_FOG_COLOR, GRISOSCURO);
+		}
+		else {
+			glFogfv(GL_FOG_COLOR, GRISCLARO);
+		}
+		glFogf(GL_FOG_DENSITY, 0.06);
+	}
+	else {
+		glDisable(GL_FOG);
+	}
+
 	updateTitulo();
+
+	glLoadIdentity();
+	if (true) {
+		//DIBUJO DEL HUD
+		glDisable(GL_LIGHTING);
+		glPushMatrix();
+		glTranslatef(0.75, -0.75, -3.0);
+		glPushMatrix();//Flecha
+		glScalef(0.8*0.5, 2.5, 1);
+		glRotatef(-90, 1.0f, 0.0f, 0.0f);
+		glRotatef(-giroz / 60, 0.0f, 0.0f, 1.0f);
+		dibujaFlecha(0.5);
+		glPopMatrix();
+		glPushMatrix();//Velocidad
+		glTranslatef(-0.75f, -0.25f, 0.0f);
+		glScalef(1.0f, 0.25f, 1.0f);
+		dibujaVelocidad();
+		glPopMatrix();
+		glPushMatrix();
+		string vel = "Velocidad";
+		char* velC = (char*)vel.c_str();
+		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+		glTranslatef(-1.75, 0.01f, 0.0f);
+		glRasterPos3f(0.0, 0.0, 0.0);
+		for (; *velC != '\0'; velC++) {
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *velC);
+		}
+		glPopMatrix();
+		glPushMatrix();
+		string dir = "Direccion";
+		char* dirC = (char*)dir.c_str();
+		glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
+		glTranslatef(-0.2f, -0.2f, 0.0f);
+		glRasterPos3f(0.0, 0.0, 0.0);
+		for (; *dirC != '\0'; dirC++) {
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *dirC);
+		}
+		glPopMatrix();
+		glPopMatrix();
+	}
 
 	glutSwapBuffers();
 }
@@ -382,6 +483,15 @@ void onKey(unsigned char tecla, int x, int y) {
 	{
 	case 'l':
 		noche = !noche;
+		break;
+	case 'L':
+		noche = !noche;
+		break;
+	case 'n':
+		niebla = !niebla;
+		break;
+	case 'N':
+		niebla = !niebla;
 		break;
 	}
 }
